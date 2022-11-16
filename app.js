266eebb2ddd21f17e944bef460cbffab02a4c7c2
alsancheezz <alsancheez@gmail.com>
2022-11-17 01:00:45 +0800

    added tie breaker function

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ const btn1 = document.querySelector("#p1Button")
 const btn2 = document.querySelector("#p2Button")
 const score = document.querySelector('.scoreHistory')
 
+btn1.disabled = true;
+btn2.disabled = true;
 
 let p1Score = 0;
 let p2Score = 0;
 let winScore = 0;
 let isGameOver = false;
+let isTiebreaker = false;
+let tieBreakerScore = 0;
 
-btn1.disabled = true;
-btn2.disabled = true;
 
 numRounds.addEventListener('change', () =>{
   winScore=parseInt((numRounds.value))
+  tieBreakerScore = winScore - 1;
   btn1.disabled = false;
   btn2.disabled = false;
   reset();
@@ -28,39 +31,75 @@ numRounds.addEventListener('change', () =>{
 })
 
 p1Button.addEventListener('click', ()=>{
- if (!isGameOver) {
-  p1Score += 1;
-  if (p1Score === winScore) {
-     isGameOver = true;
-     p1.classList.add('winner')
-     p2.classList.add('loser')
+ if (isGameOver) return;
 
-  } 
-  p1.textContent = p1Score;
+ p1Score += 1;
+
  
-  const history = document.createElement('li')
-  history.prepend(`Player 1 wins round ${(p1Score+p2Score)}!`)
-  score.prepend(history)
+if (isTiebreaker) {
+   const diff = p1Score-p2Score
+
+    if(diff===2){
+      isGameOver = true;
+      p1.classList.add('winner')
+      p2.classList.add('loser')
+    }
+  
+ } else {
+    isTiebreaker = tieBreakerScore === p1Score && tieBreakerScore === p2Score;
+
+      if (p1Score === winScore) {
+    isGameOver = true;
+    p1.classList.add('winner')
+    p2.classList.add('loser')
+ } 
+}
+
+ p1.textContent = p1Score;
+ const history = document.createElement('li')
+ history.prepend(`Player 1 wins round ${(p1Score+p2Score)}!`)
+ score.prepend(history)
+
+
  }
 
-})
+)
 
 p2Button.addEventListener('click', ()=>{
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winScore) {
-       isGameOver = true;
-       p2.classList.add('winner')
-       p1.classList.add('loser')
-    } 
-    p2.textContent = p2Score; 
-
-    const history = document.createElement('li')
-    history.prepend(`Player 2 wins round ${(p1Score+p2Score)}!`)
-    score.prepend(history)
-   }
+  if (isGameOver) return;
+ 
+  p2Score += 1;
+ 
   
-})
+ if (isTiebreaker) {
+    const diff = p2Score-p1Score
+ 
+     if(diff===2){
+      isGameOver = true;
+      p2.classList.add('winner')
+      p1.classList.add('loser')
+     }
+   
+  } else {
+     isTiebreaker = tieBreakerScore === p1Score && tieBreakerScore === p2Score;
+ 
+       if (p2Score === winScore) {
+     isGameOver = true;
+     p2.classList.add('winner')
+     p1.classList.add('loser')
+  } 
+ }
+ 
+  p2.textContent = p2Score;
+  const history = document.createElement('li')
+  history.prepend(`Player 2 wins round ${(p1Score+p2Score)}!`)
+  score.prepend(history)
+ 
+ 
+  }
+ 
+ )
+
 
 function deleteScore() {
   var e = document.querySelector("ul");
@@ -83,8 +122,4 @@ function reset() {
   deleteScore()
 }
 
-
-
 res.addEventListener('click', reset)
-
-
